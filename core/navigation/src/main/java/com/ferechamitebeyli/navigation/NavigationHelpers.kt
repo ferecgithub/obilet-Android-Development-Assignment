2265247f1cdee5adc507ef940660cf18786a4296
ferecgithub <ferechamitbeyli@gmail.com>
2024-04-04 00:31:29 +0300

    NavigationHelper extension functions are introduced.

---
+++ core/navigation/src/main/java/com/ferechamitebeyli/navigation/NavigationHelpers.kt
@@ -0,0 +1,16 @@
+package com.ferechamitebeyli.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+
+/**
+ * Created by Ferec Hamitbeyli on 4.04.2024.
+ */
+
+fun NavController.safeNavigate(direction: NavDirections) {
+    currentDestination?.getAction(direction.actionId)?.let { navigate(direction) }
+}
+
+fun NavController.safeNavigate(id: Int) {
+    currentDestination?.getAction(id)?.let { navigate(id) }
+}
