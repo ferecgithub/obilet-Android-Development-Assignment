28d739a49816e727fbcc6100c01f2df1a76d1b45
ferecgithub <ferechamitbeyli@gmail.com>
2024-04-04 00:18:23 +0300

    BaseApplication for hilt is created and introduced to AndroidManifest.

---
+++ app/src/main/java/com/ferechamitebeyli/obiletandroidassignment/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.ferechamitebeyli.obiletandroidassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ferec Hamitbeyli on 4.04.2024.
+ */
+
+@HiltAndroidApp
+class BaseApplication: Application()
