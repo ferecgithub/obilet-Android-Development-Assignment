629b343176e822aee6891abf13ee250cdb50ee1c
ferecgithub <ferechamitbeyli@gmail.com>
2024-04-05 18:43:53 +0300

    A function for getting unique hardware identifier is introduced.

--- feature/splash/src/main/java/com/ferechamitebeyli/splash/presentation/fragment/SplashFragment.kt
+++ feature/splash/src/main/java/com/ferechamitebeyli/splash/presentation/fragment/SplashFragment.kt
@@ -1,13 +1,17 @@
 package com.ferechamitebeyli.splash.presentation.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ferechamitebeyli.splash.R
 import com.ferechamitebeyli.splash.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
@@ -26,6 +30,11 @@ class SplashFragment : Fragment() {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getUniqueDeviceIdentifier(): String {
+        return Settings.Secure.getString(requireContext().contentResolver, Settings.Secure.ANDROID_ID)
     }
 
     override fun onDestroyView() {
---
+++ feature/splash/src/main/java/com/ferechamitebeyli/splash/presentation/viewmodel/SplashViewModel.kt
@@ -0,0 +1,16 @@
+package com.ferechamitebeyli.splash.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Ferec Hamitbeyli on 5.04.2024.
+ */
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
