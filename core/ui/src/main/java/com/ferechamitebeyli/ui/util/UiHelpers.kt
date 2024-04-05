f213dce9d6558ab9d62fd3a00b27f676660cb8dc
ferecgithub <ferechamitbeyli@gmail.com>
2024-04-05 17:39:37 +0300

    Coil extension function for loading logo is introduced.

--- core/ui/src/main/java/com/ferechamitebeyli/ui/util/UiHelpers.kt
+++ core/ui/src/main/java/com/ferechamitebeyli/ui/util/UiHelpers.kt
@@ -1,4 +1,15 @@
 package com.ferechamitebeyli.ui.util
 
+import android.widget.ImageView
+import coil.load
+import com.ferechamitebeyli.ui.R
+
 object UiHelpers {
+
+    fun ImageView.loadPartnerLogo(partnerId: Int) {
+        this.load(" https://s3.eu-central-1.amazonaws.com/static.obilet.com/images/partner/$partnerId-sm.png") {
+            crossfade(true)
+            placeholder(R.drawable.ic_bus)
+        }
+    }
 }
