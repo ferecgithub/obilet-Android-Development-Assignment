45cd4a45603af91c20e06d848eb556ebce2fdd28
ferecgithub <ferechamitbeyli@gmail.com>
2024-04-07 02:26:49 +0300

    BusJourneyIndexFragment RecyclerView is implemented.

--- feature/journey/src/main/java/com/ferechamitebeyli/journey/presentation/fragment/BusJourneyIndexFragment.kt
+++ feature/journey/src/main/java/com/ferechamitebeyli/journey/presentation/fragment/BusJourneyIndexFragment.kt
@@ -1,35 +1,26 @@
 package com.ferechamitebeyli.journey.presentation.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.ferechamitebeyli.journey.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ferechamitebeyli.journey.databinding.FragmentBusJourneyIndexBinding
+import com.ferechamitebeyli.journey.presentation.adapter.BusJourneyListAdapter
+import com.ferechamitebeyli.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class BusJourneyIndexFragment : Fragment() {
-    private var _binding: FragmentBusJourneyIndexBinding? = null
-    private val binding get() = _binding!!
+class BusJourneyIndexFragment : BaseFragment<FragmentBusJourneyIndexBinding>(
+    FragmentBusJourneyIndexBinding::inflate
+) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var busJourneyAdapter: BusJourneyListAdapter
+
+    override fun setUpUi() {
+        super.setUpUi()
+
+        binding.recyclerViewBusJourneyIndex.apply {
+            busJourneyAdapter = BusJourneyListAdapter(requireContext())
+            adapter = busJourneyAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentBusJourneyIndexBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
